9954ca8f7f60b6fa9ba58456a8698c002e438a75
zainisacoder <zain.hanif88@gmail.com>
2023-05-02 13:58:04 +0200

    Initial commit

---
+++ src/main/java/org/CHATAPP/Server.java
@@ -0,0 +1,93 @@
+package org.APP;
+import java.io.BufferedReader;
+import java.net.*;
+import java.io.*;
+
+
+class Server {
+    ServerSocket server;
+    Socket socket;
+
+    BufferedReader br;
+    PrintWriter out;
+
+    //constructor
+    public Server() {
+        try {
+            server = new ServerSocket(7777);
+            System.out.println("Server is ready to accept connection");
+            System.out.println("Waiting...");
+            socket = server.accept();
+
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            out = new PrintWriter(socket.getOutputStream());
+
+            startReading();
+            startWriting();
+
+
+        } catch (Exception e) {
+            //TODO:Handel Exception
+            e.printStackTrace();
+        }
+    }
+
+    public void startReading() {
+        //thread will read and throw data
+        Runnable r1 = () -> {
+            System.out.println("reader started..");
+
+            try {
+                while (true) {
+
+                    String msg = br.readLine();
+                    if (msg.equals("exit")) {
+                        System.out.println("Client terminated the chat");
+                        break;
+                    }
+
+                    System.out.println("Client : " + msg);
+                }
+            }catch (Exception e) {
+                    e.printStackTrace();
+                }
+        };
+
+            new Thread(r1).start();
+        }
+        public void startWriting ()
+        {
+            // thread will give data to user and user will send it to client
+            Runnable r2 = () -> {
+                System.out.println("Writer Started...");
+
+                try {
+                    while (true) {
+
+                        BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+                        String content = br1.readLine();
+
+                        out.println(content);
+                        out.flush();
+
+                        if(content.equals("exit"))
+                        {
+                            socket.close();
+                            break;
+                        }
+                    }
+
+                } catch (Exception e) {
+                        // TODO: handle exception
+                        e.printStackTrace();
+                    }
+            };
+            new Thread(r2).start();
+        }
+        public static void main (String[]args){
+            System.out.println("this is server. going to start server");
+            new Server();
+        }
+    }
+
